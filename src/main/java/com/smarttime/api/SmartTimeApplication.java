579130ae853bb63fcb1f2d2aa8daaee2016ef84e
Michel Tonon <michel.tonon@stoom.com.br>
2020-10-23 15:36:12 -0300

    Commit inicial

---
+++ src/main/java/com/smarttime/api/SmartTimeApplication.java
@@ -0,0 +1,13 @@
+package com.smarttime.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartdot/api/SmartTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartdot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
